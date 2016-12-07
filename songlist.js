9afbef107fabff711c287946c7009aa4aa6305fe
VEE <v_thao9@mail.csuchico.edu>
2016-12-07 15:32:19 -0800

    added displaysong() to loadlist()

--- songlist.js
+++ songlist.js
@@ -29,6 +29,7 @@ function loadList()
   allSongs.push(new Song("Get Lucky","Daft Punk","Random Access Memories",2013));
   allSongs.push(new Song("Inevitable","Shakira","Dónde Están los Ladrones?",1998));
   allSongs.push(new Song("Wagon Wheel","Old Crow Medicine Show","Old Crow Medicine Show",2004));
+  displaySong();
 }
 
 //Progresses to the next song to the list, or if the end of the list has been
